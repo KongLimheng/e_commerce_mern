33496092e4fcfbe3c4bdd70acacb3382a068c045
konglimheng <banakama12@gmail.com>
2023-05-30 14:10:08 +0700

    gitignore

--- backend/multer.js
+++ backend/multer.js
@@ -1,11 +1,16 @@
+const moment = require('moment/moment')
 const multer = require('multer')
+const path = require('path')
 
 const storage = multer.diskStorage({
     destination: (req, res, cb) => {
+        const basePath = path.resolve(__dirname)
         cb(null, 'uploads/')
     },
     filename(req, file, callback) {
-        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`
+        const uniqueSuffix = `${moment.now()}-${Math.round(
+            Math.random() * 1e9
+        )}`
 
         const filename = file.originalname.split('.')
         callback(null, `${filename[0]}-${uniqueSuffix}.${filename[1]}`)
